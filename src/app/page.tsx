4a178945da5957122bfc0347428baa313381336b
Colin Guinane <colinguinane@gmail.com>
2024-05-07 16:26:49 -0600

    bugfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import { collection, getDocs, DocumentData } from "firebase/firestore";
 import { db } from "./firebase/firebase";
