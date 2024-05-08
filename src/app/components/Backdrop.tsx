4c6c8ca2727ebeefe4549ac3d0f594dddfee0895
Colin Guinane <colinguinane@gmail.com>
2024-05-08 16:18:31 -0600

    deploy fix

--- src/app/components/Backdrop.tsx
+++ src/app/components/Backdrop.tsx
@@ -1,9 +1,6 @@
-const Backdrop = ({ onClick }) => {
+const Backdrop = ({}) => {
   return (
-    <div
-      className="fixed top-0 left-0 w-full h-full bg-gray-600 bg-opacity-50"
-      onClick={onClick}
-    ></div>
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-600 bg-opacity-50"></div>
   );
 };
 
