b1cc0dbfd84b4cf05b89667bcffdfa0167e348e7
colinguinane1 <colinguinane@gmail.com>
2024-05-22 01:19:19 -0600

    fixed height issue

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -449,29 +449,6 @@ const Modal: React.FC<ModalProps> = ({
               </motion.div>
             )}{" "}
           </AnimatePresence>
-          {/* <motion.button
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              onClick={toggleCustomize}
-              className="bg-green-500 no_transition flex items-center py-2 hover:bg-green-700 rounded-lg px-2 text-white"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-settings stroke-white"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="#000000"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
-                <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-              </svg>
-            </motion.button>{" "} */}
           <div className="md:flex items-center gap-2 md:relative w-full fixed bottom-4">
             {" "}
           </div>{" "}
@@ -482,7 +459,7 @@ const Modal: React.FC<ModalProps> = ({
               onClick={addToCart}
               className={`bg-green-500 ${
                 largeScreen ? "fixed" : ""
-              } no_transition w-full px-20 my-4 md:mb-20 hover:bg-green-700 z-[1000] rounded-lg py-3 text-white`}
+              } no_transition w-full my-4 hover:bg-green-700 z-[1000] rounded-lg py-3 text-white`}
             >
               Add +${totalPrice.toFixed(2)}
             </motion.button>
