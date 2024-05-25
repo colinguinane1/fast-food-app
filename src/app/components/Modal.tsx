0e68484d72b6cfb5c341a6516c013f40f45e94fb
colinguinane1 <colinguinane@gmail.com>
2024-05-25 01:49:39 -0600

    added dip sauce logic

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -136,7 +136,100 @@ const Modal: React.FC<ModalProps> = ({
     }
   };
 
-  // Rest of your ingredient and extra ingredient logic...
+  const updateIngredientCount = (ingredientName: string, value: number) => {
+    setIngredients((prevIngredients) => ({
+      ...prevIngredients,
+      [ingredientName]: { count: value },
+    }));
+  };
+
+  const updateExtraIngredientCount = (
+    ingredientName: string,
+    value: number
+  ) => {
+    setExtraIngredients((prevExtraIngredients) => ({
+      ...prevExtraIngredients,
+      [ingredientName]: { count: value },
+    }));
+  };
+
+  const addIngredient = (ingredientName: string) => {
+    if (
+      ingredients[ingredientName] &&
+      ingredients[ingredientName].count <
+        itemData.itemIngredients[ingredientName].max
+    ) {
+      updateIngredientCount(
+        ingredientName,
+        ingredients[ingredientName].count + 1
+      );
+      if (
+        ingredients[ingredientName].count + 1 >
+        itemData.itemIngredients[ingredientName].comesWith
+      ) {
+        setTotalPrice(
+          (prevTotalPrice) =>
+            prevTotalPrice + itemData.itemIngredients[ingredientName].price
+        );
+      }
+    }
+  };
+
+  const removeIngredient = (ingredientName: string) => {
+    if (
+      ingredients[ingredientName] &&
+      ingredients[ingredientName].count >
+        itemData.itemIngredients[ingredientName].min
+    ) {
+      updateIngredientCount(
+        ingredientName,
+        ingredients[ingredientName].count - 1
+      );
+      if (
+        ingredients[ingredientName].count >
+        itemData.itemIngredients[ingredientName].comesWith
+      ) {
+        setTotalPrice(
+          (prevTotalPrice) =>
+            prevTotalPrice - itemData.itemIngredients[ingredientName].price
+        );
+      }
+    }
+  };
+
+  const addExtraIngredient = (ingredientName: string) => {
+    if (
+      extraIngredients[ingredientName] &&
+      extraIngredients[ingredientName].count <
+        itemData.itemExtraIngredients[ingredientName].max
+    ) {
+      updateExtraIngredientCount(
+        ingredientName,
+        extraIngredients[ingredientName].count + 1
+      );
+      setTotalPrice(
+        (prevTotalPrice) =>
+          prevTotalPrice + itemData.itemExtraIngredients[ingredientName].price
+      );
+    }
+  };
+
+  const removeExtraIngredient = (ingredientName: string) => {
+    if (
+      extraIngredients[ingredientName] &&
+      extraIngredients[ingredientName].count >
+        itemData.itemExtraIngredients[ingredientName].min
+    ) {
+      updateExtraIngredientCount(
+        ingredientName,
+        extraIngredients[ingredientName].count - 1
+      );
+      setTotalPrice(
+        (prevTotalPrice) =>
+          prevTotalPrice - itemData.itemExtraIngredients[ingredientName].price
+      );
+    }
+  };
 
   return (
     <motion.main
@@ -394,80 +487,103 @@ const Modal: React.FC<ModalProps> = ({
                     )}
                   </div>
                 )}
-                {Object.entries(itemData.itemDip.availableDips).map(
-                  ([dipName, dip]) => (
-                    <div
-                      key={dipName}
-                      className="flex flex-row py-3 items-center justify-between"
-                    >
-                      <p
-                        className={`capitalize ${
-                          dips[dipName]?.count > 0 ? "text-green-500" : ""
-                        }`}
-                      >
-                        {dipName}
-                        {dips[dipName]?.count > 1
-                          ? ` x${dips[dipName].count}`
-                          : ""}
+                {Object.entries(itemData.itemDip.availableDips).length > 0 && (
+                  <div className="">
+                    <div className="flex items-center gap-4">
+                      <h2 className="font-extrabold text-xl py-1">Dip</h2>
+                      <p className="text-sm italic">
+                        (Choose up to {itemData.itemDip.maxDips})
                       </p>
-                      <div className="">
-                        <h1 className="text-gray-200">
-                          +${dip.price ? dip.price.toFixed(2) : ""}
-                        </h1>
-                      </div>
-                      <div className="flex gap-2 items-center">
-                        <motion.button
-                          whileHover={{ scale: 1.1 }}
-                          whileTap={{ scale: 0.9 }}
-                          onClick={() => addDip(dipName)}
-                          className="no_transition"
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className={`icon icon-tabler icon-tabler-plus stroke-white rounded-lg w-7 h-7 ${
-                              dips[dipName]?.count === dip.max
-                                ? "bg-gray-200 stroke-slate-300 cursor-not-allowed"
-                                : "bg-green-500"
-                            }`}
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="#ffffff"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M12 5v14" />
-                            <path d="M5 12h14" />
-                          </svg>
-                        </motion.button>
-                        <motion.button
-                          whileHover={{ scale: 1.1 }}
-                          whileTap={{ scale: 0.9 }}
-                          className="no_transition"
-                          onClick={() => removeDip(dipName)}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className={`icon icon-tabler icon-tabler-minus stroke-white rounded-lg w-7 h-7 ${
-                              dips[dipName]?.count === dip.min
-                                ? "bg-gray-200 stroke-slate-300 cursor-not-allowed"
-                                : "bg-green-500"
-                            }`}
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="#ffffff"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M5 12h14" />
-                          </svg>
-                        </motion.button>
-                      </div>
                     </div>
-                  )
+                    {Object.entries(itemData.itemDip.availableDips).map(
+                      ([dipName, dip]) => (
+                        <div
+                          key={dipName}
+                          className="flex flex-row py-3 items-center justify-between"
+                        >
+                          <p
+                            className={`capitalize ${
+                              dips[dipName]?.count > 0 ? "text-green-500" : ""
+                            }`}
+                          >
+                            {dipName}
+                            {dips[dipName]?.count > 1
+                              ? ` x${dips[dipName].count}`
+                              : ""}
+                          </p>
+                          <div className="">
+                            <h1 className="text-gray-200">
+                              {/* +${dip.price ? dip.price.toFixed(2) : ""} */}
+                            </h1>
+                          </div>
+                          <div className="flex gap-2 items-center">
+                            <motion.button
+                              whileHover={{ scale: 1.1 }}
+                              whileTap={{ scale: 0.9 }}
+                              onClick={() => addDip(dipName)}
+                              className="no_transition"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className={`icon icon-tabler icon-tabler-plus stroke-white rounded-lg w-7 h-7 ${
+                                  dips[dipName]?.count === dip.max
+                                    ? "bg-gray-200 stroke-slate-300 cursor-not-allowed"
+                                    : "bg-green-500"
+                                }`}
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="#ffffff"
+                                fill="none"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              >
+                                <path
+                                  stroke="none"
+                                  d="M0 0h24v24H0z"
+                                  fill="none"
+                                />
+                                <path d="M12 5v14" />
+                                <path d="M5 12h14" />
+                              </svg>
+                            </motion.button>
+                            <motion.button
+                              whileHover={{ scale: 1.1 }}
+                              whileTap={{ scale: 0.9 }}
+                              className="no_transition"
+                              onClick={() => removeDip(dipName)}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className={`icon icon-tabler icon-tabler-minus stroke-white ${
+                                  dips[dipName].count ==
+                                  itemData.itemDip.maxDips
+                                    ? "bg-gray-200"
+                                    : ""
+                                } rounded-lg w-7 h-7 ${
+                                  dips[dipName]?.count === dip.min
+                                    ? "bg-gray-200 stroke-slate-300 cursor-not-allowed"
+                                    : "bg-green-500"
+                                }`}
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="#ffffff"
+                                fill="none"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              >
+                                <path
+                                  stroke="none"
+                                  d="M0 0h24v24H0z"
+                                  fill="none"
+                                />
+                                <path d="M5 12h14" />
+                              </svg>
+                            </motion.button>
+                          </div>
+                        </div>
+                      )
+                    )}
+                  </div>
                 )}
               </motion.div>
             )}
