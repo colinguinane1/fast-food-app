5d11c8198e96c2564e5474a6b726403c034d0a83
colinguinane1 <colinguinane@gmail.com>
2024-07-06 16:29:43 -0600

    added icon

--- src/app/components/Checkout.tsx
+++ src/app/components/Checkout.tsx
@@ -67,7 +67,27 @@ const Checkout: React.FC<CheckoutProps> = ({ toggleCartVisible }) => {
       {cartCount > 0 ? (
         <>
           <div className="p-4">
-            <h1 className="font-extrabold text-4xl py-4">Checkout</h1>
+            <h1 className=" flex  items-center  gap-2 font-extrabold text-4xl py-4">
+              Checkout{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-shopping-cart stroke-black"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#000000"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                <path d="M17 17h-11v-14h-2" />
+                <path d="M6 5l14 1l-1 7h-13" />
+              </svg>
+            </h1>
 
             <ul className="py-4 flex flex-col gap-6">
               {cartContents.map((item, index) => (
