ef98af8c69f91e11b2085ed2b4f7ab29c4cb5931
colinguinane1 <colinguinane@gmail.com>
2024-07-08 10:23:29 -0600

    ui fixes and fixed removeCart modal

--- src/app/components/Checkout.tsx
+++ src/app/components/Checkout.tsx
@@ -73,14 +73,35 @@ const Checkout: React.FC<CheckoutProps> = ({ toggleCartVisible }) => {
     setCartRemove(!cartRemove);
   };
 
+  const toggleCancelCartRemove = () => {
+    setCartRemove(!cartRemove);
+  };
+
   return (
     <main className="z-10 overflow-y-auto pb-20 md:pb-4 fixed bg-gradient-to-b from-slate-200 to-white w-screen h-screen inset-0">
       <div className="flex flex-col items-center "></div>
       {cartCount > 0 ? (
         <>
           <div className="p-2 flex flex-col items-center w-full  justify-center ">
-            <div className="max-w-[70rem]">
+            <div className="max-w-[40rem] w-full">
               <div className="flex justify-between">
+                <button className="block" onClick={toggleCartVisible}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-chevron-down"
+                    width="44"
+                    height="44"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="#2c3e50"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M6 9l6 6l6 -6" />
+                  </svg>
+                </button>{" "}
                 <h1 className=" flex  items-center   gap-2 font-extrabold text-4xl py-4">
                   Checkout{" "}
                   <svg
@@ -102,23 +123,6 @@ const Checkout: React.FC<CheckoutProps> = ({ toggleCartVisible }) => {
                     <path d="M6 5l14 1l-1 7h-13" />
                   </svg>
                 </h1>
-                <button className="block" onClick={toggleCartVisible}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon icon-tabler icon-tabler-chevron-down"
-                    width="44"
-                    height="44"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="#2c3e50"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M6 9l6 6l6 -6" />
-                  </svg>
-                </button>
               </div>
 
               <ul className="py-4 flex flex-col gap-6">
@@ -318,52 +322,58 @@ const Checkout: React.FC<CheckoutProps> = ({ toggleCartVisible }) => {
           </div>
         </div>
       )}
-      {cartRemove && selectedItem && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.2 }}
-          className="fixed no_transition top-0 h-screen w-screen bg-black bg-opacity-40"
-        >
-          {" "}
-          <div className="flex items-center justify-center">
-            <motion.div
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              exit={{ scale: 0 }}
-              transition={{ delay: 0.2 }}
-              className="flex no_transition flex-col text-left  w-60 mt-[50%] p-2 px-4 bg-white rounded-lg"
-            >
-              <div>
-                {" "}
-                <h1 className="font-semibold text-xl py-3">
-                  Remove from Cart?
-                </h1>
-                <p>{selectedItem.name}</p>
-              </div>
+      <AnimatePresence>
+        {cartRemove && selectedItem && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="fixed no_transition top-0 h-screen w-screen bg-black bg-opacity-40"
+          >
+            {" "}
+            <div className="flex items-center justify-center">
+              <motion.div
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}
+                transition={{ delay: 0.2 }}
+                className="flex no_transition flex-col text-left  w-60 mt-[50%] p-2 px-4 bg-white rounded-lg"
+              >
+                <div>
+                  {" "}
+                  <h1 className="font-semibold text-xl py-3">
+                    Remove from Cart?
+                  </h1>
+                  <p>{selectedItem.name}</p>
+                </div>
 
-              <div className="flex py-3 justify-between gap-4">
-                <button
-                  onClick={() => toggleCartRemove}
-                  className="bg-slate-200 font-semibold p-2 rounded-lg w-full"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={() => {
-                    removeFromCart(selectedItem);
-                    toggleCartRemove();
-                  }}
-                  className="bg-red-500 text-white font-semibold p-2 rounded-lg w-full"
-                >
-                  Remove
-                </button>
-              </div>
-            </motion.div>
-          </div>
-        </motion.div>
-      )}
+                <div className="flex py-3 justify-between gap-4">
+                  <motion.button
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    onClick={toggleCancelCartRemove}
+                    className="no_transition bg-slate-200 font-semibold p-2 rounded-lg w-full"
+                  >
+                    Cancel
+                  </motion.button>
+                  <motion.button
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    onClick={() => {
+                      removeFromCart(selectedItem);
+                      toggleCartRemove();
+                    }}
+                    className="bg-red-500 no_transition text-white font-semibold p-2 rounded-lg w-full"
+                  >
+                    Remove
+                  </motion.button>
+                </div>
+              </motion.div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 };
